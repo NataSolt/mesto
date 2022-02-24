f18c9fa514ca827224651eb191e195ae21413adb
Natalia Sokolova <solta@inbox.ru>
2022-02-24 21:28:11 +0300

    первая правка

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
 let profileAddButton = document.querySelector(".profile__add-button");
 let popup = document.querySelector(".popup");
 let closePopupButton = document.querySelector(".popup__close");
-let popupContainer = document.querySelector(".popup__container");
-let inputName = document.querySelector(".popup__name");
-let inputJob = document.querySelector(".popup__job");
+let popupContainer = document.querySelector(".popup__form");
+let inputName = document.querySelector(".popup__text_type_name");
+let inputJob = document.querySelector(".popup__text_type_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 
-profileAddButton.addEventListener("click", addPopup);
 function addPopup() {
   popup.classList.add("popup_opened");
-}
-
-closePopupButton.addEventListener("click", removePopup);
-function removePopup() {
-  popup.classList.remove("popup_opened");
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
+}
+
+function removePopup() {
+  popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
@@ -26,4 +24,6 @@ function formSubmitHandler(evt) {
   removePopup();
 }
 
+closePopupButton.addEventListener("click", removePopup);
+profileAddButton.addEventListener("click", addPopup);
 popupContainer.addEventListener("submit", formSubmitHandler);
