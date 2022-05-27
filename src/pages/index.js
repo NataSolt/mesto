4782b0d119a49f89b2b38797adf945692954d72a
Natalia Sokolova <solta@inbox.ru>
2022-05-27 13:29:18 +0300

    правка размера кнопки удалить

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import { Card } from "../components/Card.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { config } from "../utils/constants.js";
-import { Section } from "../components/Section.js";
+import { Section } from "../components/Section.js/index.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
